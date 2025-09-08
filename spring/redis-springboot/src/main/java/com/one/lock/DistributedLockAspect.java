0bfdd6eed7ac0b0336f52a35ab57c5d7ca6b5483
wanjunjie <ogo1828wan@163.com>
2025-09-09 00:04:29 +0800

    学习Caffeine

--- spring/redis-springboot/src/main/java/com/one/lock/DistributedLockAspect.java
+++ spring/redis-springboot/src/main/java/com/one/lock/DistributedLockAspect.java
@@ -1,7 +1,7 @@
 package com.one.lock;
 
-import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
