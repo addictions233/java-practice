d687b9c61f7c784b3780ef1cd611472adc8135ff
wanjunjie <ogo1828wan@163.com>
2025-09-13 17:29:17 +0800

    学习Caffeine

---
+++ spring/springboot-job/src/main/java/com/one/job/timer/TimerDemo.java
@@ -0,0 +1,24 @@
+package com.one.job.timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Timer timer = new Timer();
+        // 启动定时任务
+        timer.schedule(new TimerTaskDemo(), 1000, 1000);
+        Thread.sleep(10000);
+        // 取消定时任务
+        timer.cancel();
+    }
+
+
+    static class TimerTaskDemo extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("TimerTaskDemo run");
+        }
+    }
+}
