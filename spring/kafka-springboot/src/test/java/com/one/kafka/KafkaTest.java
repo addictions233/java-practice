8b5a7dc37cd7f695b5f2785692b92f641ca4d165
wanjunjie <ogo1828wan@163.com>
2025-12-06 16:44:45 +0800

    学习kafka

--- spring/kafka-springboot/src/test/java/com/one/kafka/KafkaTest.java
+++ spring/kafka-springboot/src/test/java/com/one/kafka/KafkaTest.java
@@ -4,7 +4,6 @@ import com.one.kafla.db.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.security.RunAs;
 
 @SpringBootTest
 public class KafkaTest {
