e44eef8ce9a0b8e1aac84ed87541a5fdfafa947f
wanjunjie <ogo1828wan@163.com>
2025-12-09 22:13:09 +0800

    学习kafka

--- spring/kafka-demo/src/main/java/com/one/idempotence/MyProducer2.java
+++ spring/kafka-demo/src/main/java/com/one/idempotence/MyProducer2.java
@@ -8,13 +8,13 @@ import java.util.concurrent.ExecutionException;
 /**
  * @author one
  * 这里首先需要理解分布式数据传递过程中的三个数据语义：
- *           at-least-once:至少一次；
- *           at-most-once:最多一次；
- *           exactly-once:精确一次。
+ * at-least-once:至少一次；
+ * at-most-once:最多一次；
+ * exactly-once:精确一次。
  * 演示生产者消息幂等性问题
  */
 public class MyProducer2 {
-//    private static final String BOOTSTRAP_SERVERS = "worker1:9092,worker2:9092,worker3:9092";
+    //    private static final String BOOTSTRAP_SERVERS = "worker1:9092,worker2:9092,worker3:9092";
     private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
     private static final String TOPIC = "kafka-one";
 
@@ -24,13 +24,13 @@ public class MyProducer2 {
         // 此处配置的是kafka的端口
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         // 配置key的序列化类
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         // 配置value的序列化类
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String,String> producer1 = new KafkaProducer<>(props);
-        Producer<String,String> producer2 = new KafkaProducer<>(props);
-        for(int i = 0; i < 5; i++) {
+        Producer<String, String> producer1 = new KafkaProducer<>(props);
+        Producer<String, String> producer2 = new KafkaProducer<>(props);
+        for (int i = 0; i < 5; i++) {
             ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, Integer.toString(i), "MyProducer" + i);
             //同步发送：获取服务端应答消息前，会阻塞当前线程。
             RecordMetadata recordMetadata = producer1.send(record).get();
@@ -38,14 +38,14 @@ public class MyProducer2 {
             int partition = recordMetadata.partition();
             long offset = recordMetadata.offset();
             String message = recordMetadata.toString();
-            System.out.println("message:["+ message+"] sended with topic:"+topic+"; partition:"+partition+ ";offset:"+offset);
+            System.out.println("message:[" + message + "] sended with topic:" + topic + "; partition:" + partition + ";offset:" + offset);
 
             RecordMetadata recordMetadata2 = producer2.send(record).get();
             String topic2 = recordMetadata2.topic();
             int partition2 = recordMetadata2.partition();
             long offset2 = recordMetadata2.offset();
             String message2 = recordMetadata2.toString();
-            System.out.println("message2:["+ message2+"] sended with topic:"+topic2+"; partition:"+partition2+ ";offset:"+offset2);
+            System.out.println("message2:[" + message2 + "] sended with topic:" + topic2 + "; partition:" + partition2 + ";offset:" + offset2);
         }
         //消息处理完才停止发送者。
         producer1.close();
--- spring/kafka-demo/src/main/java/com/one/idempotence/RedisConsumer.java
+++ spring/kafka-demo/src/main/java/com/one/idempotence/RedisConsumer.java
@@ -22,6 +22,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * 基于Redis管理消费者Offset,防止消息重复处理。
  * 伪代码，就不去具体实现了。
+ *
  * @auther one
  */
 public class RedisConsumer {
@@ -29,7 +30,7 @@ public class RedisConsumer {
     private RedisTemplate redisTemplate;
     Logger logger = LoggerFactory.getLogger(RedisConsumer.class);
     //计算密集型任务
-    private final static int CORES = 2* Runtime.getRuntime().availableProcessors();
+    private final static int CORES = 2 * Runtime.getRuntime().availableProcessors();
     public static final String REDIS_PREFEX = "myoffset_";
 
     private volatile boolean IF_SLEEP = false;
@@ -40,18 +41,19 @@ public class RedisConsumer {
     private String servers;
     private String topic;
     private String group;
-    private final KafkaConsumer<String,String> consumer;
+    private final KafkaConsumer<String, String> consumer;
 
-    public RedisConsumer(String servers,String topic,String group){
+    public RedisConsumer(String servers, String topic, String group) {
         this.servers = servers;
         this.topic = topic;
         this.group = group;
         executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(CORES,
-                new ThreadFactory(){
+                new ThreadFactory() {
                     private final AtomicInteger threadNumber = new AtomicInteger();
+
                     @Override
                     public Thread newThread(Runnable r) {
-                        return new Thread(null,r,"RedisConsumer_"+threadNumber.getAndIncrement());
+                        return new Thread(null, r, "RedisConsumer_" + threadNumber.getAndIncrement());
                     }
                 });
         Properties props = new Properties();
@@ -65,47 +67,47 @@ public class RedisConsumer {
         props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 500);//
         props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 1);
         props.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, 64 * 1024);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG,group);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, group);
         consumer = new KafkaConsumer<>(props);
         consumer.subscribe(Arrays.asList(topic.split(",")));
     }
 
-    public void doTask(){
-        try{
-            while (RUNNING){
-                try{
-                    if(!IF_SLEEP){
+    public void doTask() {
+        try {
+            while (RUNNING) {
+                try {
+                    if (!IF_SLEEP) {
                         ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
-                        records.partitions().forEach(partition ->{
+                        records.partitions().forEach(partition -> {
                             //从redis获取偏移量
                             String redisKafkaOffset = redisTemplate.opsForHash().get(REDIS_PREFEX + partition.topic(),
                                     "" + partition.partition()).toString();
-                            long redisOffset = (null==redisKafkaOffset||"".equals(redisKafkaOffset))?-1:Long.valueOf(redisKafkaOffset);
+                            long redisOffset = (null == redisKafkaOffset || "".equals(redisKafkaOffset)) ? -1 : Long.valueOf(redisKafkaOffset);
                             List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
                             logger.info("[pull partition] topic:{}, partition:{}, records size:{}", partition.topic(),
                                     partition.partition(), partitionRecords.size());
-                            partitionRecords.forEach(record ->{
+                            partitionRecords.forEach(record -> {
                                 //redis记录的偏移量>=kafka实际的偏移量，表示已经消费过了，则丢弃。
-                                if(redisOffset >= record.offset()){
+                                if (redisOffset >= record.offset()) {
                                     logger.error("[pool discard] group id:{}, offset:{}, redisOffset:{} ,value:{}", group, record.offset(), redisOffset, record.value());
                                     return;
                                 }
-                                executorService.execute(()->{
-                                    doMessage(record.topic(),record.value());
+                                executorService.execute(() -> {
+                                    doMessage(record.topic(), record.value());
                                 });
                             });
                             //保存Redis偏移量
-                            long saveRedisOffset = partitionRecords.get(partitionRecords.size()-1).offset();
-                            redisTemplate.opsForHash().put(REDIS_PREFEX + partition.topic(),"" + partition.partition(),saveRedisOffset);
+                            long saveRedisOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
+                            redisTemplate.opsForHash().put(REDIS_PREFEX + partition.topic(), "" + partition.partition(), saveRedisOffset);
                         });
                         //异步提交。消费业务多时，异步提交有可能造成消息重复消费，通过Redis中的Offset，就可以过滤掉这一部分重复的消息。
                         consumer.commitAsync();
                     }
-                }catch (Throwable e) {
+                } catch (Throwable e) {
                     logger.warn("[consumer exception] {}", e);
                 }
             }
-        }catch (Throwable e) {
+        } catch (Throwable e) {
             logger.warn("[huge exception] to finish. {}", e);
         } finally {
             executorService.shutdown();
@@ -120,8 +122,9 @@ public class RedisConsumer {
             logger.warn("[finish consumer] topic:{}, groupId:{}.", topic, group);
         }
     }
+
     //实际处理请求。通常可以交给子实现类去做。
-    private void doMessage(String topic,String value){
-        System.out.println("[deal message] topic : "+topic + "; value = "+value);
+    private void doMessage(String topic, String value) {
+        System.out.println("[deal message] topic : " + topic + "; value = " + value);
     }
 }
