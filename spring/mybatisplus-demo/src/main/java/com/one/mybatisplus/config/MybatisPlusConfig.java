75243db5ea133aacfc8720fcc44a2f0a6569bc3c
wanjunjie <ogo1828wan@163.com>
2025-12-10 00:06:17 +0800

    学习mybatis-plus

--- spring/mybatisplus-demo/src/main/java/com/one/mybatisplus/config/MybatisPlusConfig.java
+++ spring/mybatisplus-demo/src/main/java/com/one/mybatisplus/config/MybatisPlusConfig.java
@@ -2,7 +2,6 @@ package com.one.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +15,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         interceptor.addInnerInterceptor(new ContextParamInterceptor());
         return interceptor;
     }
