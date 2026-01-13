2ac12f015d602e78a307a29ccca3795c3a672ac0
wanjunjie <ogo1828wan@163.com>
2026-01-13 23:30:11 +0800

    学习spring

---
+++ spring/spring-aop/src/test/java/com/one/AopTest.java
@@ -0,0 +1,16 @@
+package com.one;
+
+import com.one.service.UserService;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class AopTest {
+
+    @Test
+    public void testAop() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AopApp.class);
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.save();
+    }
+}
