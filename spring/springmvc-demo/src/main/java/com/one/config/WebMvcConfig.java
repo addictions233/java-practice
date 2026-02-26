0af5485b0da27637f44e52d4162f02c7941fcb24
wanjunjie <ogo1828wan@163.com>
2026-02-27 00:44:51 +0800

    学习springmvc

--- spring/springmvc-demo/src/main/java/com/one/config/WebMvcConfig.java
+++ spring/springmvc-demo/src/main/java/com/one/config/WebMvcConfig.java
@@ -1,14 +1,20 @@
 package com.one.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+/**
+ * @author one
+ * 采用零配置的方式来使用springmvc, 可以添加一些自定义的springmvc核心组件
+ */
 @Configuration
 @EnableWebMvc // 在配置类上添加 @EnableWebMvc 注解, 会自动注册常用的 HandlerMapping 和 HandlerAdapter
+@ComponentScan("com.one.controller")
 public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver() {
