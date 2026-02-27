6e8078fd1996c6b0e4b47721ad1d95ab9f429e0c
wanjunjie <ogo1828wan@163.com>
2026-02-27 23:10:40 +0800

    学习springmvc

--- spring/springmvc-demo/src/main/java/com/one/MyWebApplicationInitializer.java
+++ spring/springmvc-demo/src/main/java/com/one/MyWebApplicationInitializer.java
@@ -4,6 +4,7 @@ import com.one.config.WebMvcConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,17 +14,22 @@ import javax.servlet.ServletRegistration;
  * @author one
  * 使用Tomcat提供的SPI机制, 替代web.xml配置文件. 来使用零配置方式启动springmvc
  * 采用SPI机制启动需要删除web.xml配置文件, 否则Tomcat还是会优先读web.xml文件
+ * springmvc 零xml配置提供的默认实现是 AbstractAnnotationConfigDispatcherServletInitializer
+ * springboot 提供的默认实现是 SpringBootServletInitializer
  */
 public class MyWebApplicationInitializer implements WebApplicationInitializer {
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
-        webApplicationContext.register(WebMvcConfig.class);
+    public void onStartup(ServletContext servletContext) {
 
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
-        ServletRegistration.Dynamic registration = servletContext.addServlet("DispatcherServlet", dispatcherServlet);
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/*");
+        // Load Spring web application configuration
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebMvcConfig.class);
+
+        // Create and register the DispatcherServlet
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1); // 设置DispatcherServlet在容器启动时就加载
+        registration.addMapping("/app/*"); // 设置DispatcherServlet处理的路径
     }
 }
