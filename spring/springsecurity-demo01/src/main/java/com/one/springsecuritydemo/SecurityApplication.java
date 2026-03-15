4322668cdd6d0602f9905487c66bcbc34d75f828
wanjunjie <ogo1828wan@163.com>
2026-03-16 00:26:33 +0800

    学习springboot

--- spring/springsecurity-demo01/src/main/java/com/one/springsecuritydemo/SecurityApplication.java
+++ spring/springsecurity-demo01/src/main/java/com/one/springsecuritydemo/SecurityApplication.java
@@ -3,7 +3,6 @@ package com.one.springsecuritydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 /**
@@ -13,10 +12,10 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 //@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })  // 关闭spring security
 @SpringBootApplication
 //@EnableWebSecurity  // 在 Spring Boot 2.7+ / 3.x 项目中，不需要显式添加 @EnableWebSecurity
-public class MainApplication {
+public class SecurityApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SecurityApplication.class, args);
 		// 获取所有的默认的过滤器链
 		DefaultSecurityFilterChain filterChains = context.getBean(DefaultSecurityFilterChain.class);
 
--- spring/springsecurity-demo02/src/main/java/com/one/SecurityApplication.java
+++ spring/springsecurity-demo02/src/main/java/com/one/SecurityApplication.java
@@ -2,14 +2,13 @@ package com.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * 主程序类
  */
 @SpringBootApplication
-public class MainApplication {
+public class SecurityApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class,args);
+        SpringApplication.run(SecurityApplication.class,args);
     }
 }
