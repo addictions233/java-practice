34cd66ac93011df7f6ee2d5c5f88eecbd668859d
wanjunjie <ogo1828wan@163.com>
2026-03-25 00:18:37 +0800

    学习springboot

--- spring/springboot-day01/springboot-mvc/src/main/java/com/com/springbootmvc/SpringbootMvcApplication.java
+++ spring/springboot-day01/springboot-mvc/src/main/java/com/com/springbootmvc/SpringbootMvcApplication.java
@@ -2,8 +2,10 @@ package com.com.springbootmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 开启Servlet注解扫描
 public class SpringbootMvcApplication {
 
     public static void main(String[] args) {
