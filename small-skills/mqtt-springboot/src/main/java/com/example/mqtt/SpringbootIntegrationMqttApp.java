99a3356a0b7995de835793efa848d76df887e884
wanjunjie <ogo1828wan@163.com>
2026-06-30 00:17:23 +0800

    学习websocket

--- small-skills/mqtt-springboot/src/main/java/com/example/mqtt/SpringbootIntegrationMqttApp.java
+++ small-skills/mqtt-springboot/src/main/java/com/example/mqtt/SpringbootIntegrationMqttApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootIntegrationMqttDemoApplication {
+public class SpringbootIntegrationMqttApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootIntegrationMqttDemoApplication.class, args);
+        SpringApplication.run(SpringbootIntegrationMqttApp.class, args);
     }
 
 }
